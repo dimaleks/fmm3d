01779eef6a3b31ea9de1d1decc4da9ecf3ded722
Dmitry Alexeev <alexeedm@ethz.ch>
2016-08-15 12:33:53 -0500

    magical profiler, not working perfectly

--- profiler.h
+++ profiler.h
@@ -11,9 +11,12 @@
 
 #include <string>
 #include <sstream>
+#include <iostream>
 #include <iomanip>
 #include <unordered_map>
 #include <algorithm>
+#include <tuple>
+#include <vector>
 
 #include "timer.h"
 
@@ -27,23 +30,131 @@ struct Timings
     Timings() : started(false), iterations(0), total(0), timer() {};
 };
 
+namespace detail
+{
+	template <typename _Fn, typename _Tuple, std::size_t... _Idx>
+	constexpr decltype(auto)
+	__apply_impl(_Fn&& f, _Tuple&& t, std::index_sequence<_Idx...>)
+	{ return std::forward<_Fn>(f)(std::get<_Idx>(std::forward<_Tuple>(t))...); }
+	
+	template <typename _Fn, typename _Tuple>
+	constexpr decltype(auto)
+	applyAllButOne(_Fn&& f, _Tuple&& t)
+	{
+		using _Indices =
+		std::make_index_sequence<std::tuple_size<std::decay_t<_Tuple>>::value - 1>;
+		return __apply_impl(std::forward<_Fn>(f), std::forward<_Tuple>(t),
+							_Indices{});
+	}
+	
+	template <typename _Fn, typename _Tuple>
+	constexpr decltype(auto)
+	apply(_Fn&& f, _Tuple&& t)
+	{
+		using _Indices =
+		std::make_index_sequence<std::tuple_size<std::decay_t<_Tuple>>::value>;
+		return __apply_impl(std::forward<_Fn>(f), std::forward<_Tuple>(t),
+							_Indices{});
+	}
+}
+
 class Profiler
-{    
-    std::unordered_map<std::string, Timings> timings;
-    Timings *ongoing;
-    
+{
+    std::unordered_map<std::string, Timings>  timings;
+	std::unordered_map<std::string, Profiler> childProfilers;
+	std::string ongoing;
+	
+	std::string __printStatAndReset(Unit unit = Unit::ms, std::string prefix)
+	{
+		double total = 0;
+		int longest = 0;
+		std::ostringstream out;
+		
+		for (auto &tm : timings)
+		{
+			if (tm.second.started)
+			{
+				tm.second.started = false;
+				tm.second.total += tm.second.timer.elapsed();
+			}
+			
+			total += (double)tm.second.total;
+			if (longest < tm.first.length())
+				longest = tm.first.length();
+		}
+		
+		double factor = 1.0;
+		std::string suffix;
+		switch (unit)
+		{
+			case Unit::s :
+				factor = 1.0e-9;
+				suffix = "s";
+				break;
+				
+			case Unit::ms :
+				factor = 1.0e-6;
+				suffix = "ms";
+				break;
+				
+			case Unit::us :
+				factor = 1.0e-3;
+				suffix = "us";
+				break;
+		}
+		longest = std::max(longest, 6);
+		
+		out << "Total time: " << std::fixed << std::setprecision(1) << total*factor << " " << suffix << std::endl;
+		out << std::left << "[" << std::setw(longest) << "Kernel" << "]    " << std::setw(20)
+		<< "Time, "+suffix << std::setw(20) << "Executions" << std::setw(20) << "Percentage" << std::endl;
+		
+		std::vector<std::pair<std::string, Timings>> v(timings.begin(), timings.end());
+		std::sort(v.begin(), v.end(), [] (auto& a, auto& b) { return a.second.total > b.second.total; });
+		
+		for (auto &tm : v)
+		{
+			out << "[" << std::setw(longest) << tm.first << "]    "
+			<< std::fixed << std::setprecision(3) << std::setw(20) << tm.second.total * factor / tm.second.iterations
+			<<  "x" << std::setw(19) << tm.second.iterations
+			<< std::fixed << std::setprecision(1) << std::setw(20) << tm.second.total / total * 100.0 << std::endl;
+		}
+		
+		timings.clear();
+		
+		if (childProfilers.size() > 0)
+		{
+			for (auto& p : childProfilers)
+			{
+				out << std::endl << "Details for " << p.first << ":" << std::endl;
+				out << p.second.printStatAndReset(unit);
+			}
+		}
+		
+		return out.str();
+	}
+	
 public:
     enum Unit {s, ms, us};
 
-    inline Profiler() : ongoing(nullptr) {};
+    inline Profiler() : ongoing("") {};
+	
+	inline void start(std::string name)
+	{
+		ongoing = name;
+		auto& tm = timings[name];
+		tm.started = true;
+		tm.timer.start();
+	}
 
-    inline void start(std::string name)
+	template<typename... Args>
+	inline void start(std::string name, Args... otherNames)
     {
-    	ongoing = &timings[name];
-    	ongoing->started = true;
-    	ongoing->timer.start();
+		start(name);
+		auto& chProf = childProfilers[name];
+		chProf.start(otherNames...);
     }
 
+	
     inline void stop(std::string name)
     {
     	if (timings.find(name) != timings.end())
@@ -56,29 +167,34 @@ public:
     			tm.iterations++;
     		}
     	}
+		ongoing = "";
+		
+		if (childProfilers.find(name) != childProfilers.end())
+		{
+			auto& chProf = childProfilers[name];
+			chProf.stop();
+		}
     }
-    
+	
     inline void stop()
     {
-    	if (ongoing != nullptr && ongoing->started)
-    	{
-    		ongoing->started = false;
-    		ongoing->total += ongoing->timer.elapsedAndReset();
-    		ongoing->iterations++;
-    	}
-    }
+		if (ongoing.length() > 0) stop(ongoing);
+	}
+	
 
-    template<typename Lambda>
-    inline void profile(std::string name, Lambda func)
+    template<typename... Args>
+    inline void profile(Args... args)
     {
-    	start(name);
-    	func();
+		auto argTp = std::make_tuple(args...);
+		detail::applyAllButOne([&] (auto... params) { this->start(params...); }, argTp);
+		auto func = std::get<std::tuple_size<std::decay_t<decltype(argTp)>>::value - 1>(argTp);
+		(func)();
     	stop();
     }
 
     inline double elapsed(std::string name, Unit unit = Unit::ms)
     {
-    	double factor = 1.0;;
+    	double factor = 1.0;
     	switch (unit)
     	{
     		case Unit::s :
@@ -158,6 +274,15 @@ public:
     	}
 
     	timings.clear();
+		
+		if (childProfilers.size() > 0)
+		{
+			for (auto& p : childProfilers)
+			{
+				out << std::endl << "Details for " << p.first << ":" << std::endl;
+				out << p.second.printStatAndReset(unit);
+			}
+		}
 
     	return out.str();
     }
